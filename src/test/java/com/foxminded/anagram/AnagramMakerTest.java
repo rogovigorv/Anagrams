cc459cb460a26147789a555986c7ba3808410302
Игорь Рогов <rogovigor89@mail.ru>
2020-07-30 20:36:28 +0300

    Implemented Switch to JUnit5

--- src/test/java/com/foxminded/anagram/AnagramMakerTest.java
+++ src/test/java/com/foxminded/anagram/AnagramMakerTest.java
@@ -1,105 +1,103 @@
 package com.foxminded.anagram;
 
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import static org.junit.Assert.assertEquals;
+import org.hamcrest.Matcher;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AnagramMakerTest {
-
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
 
     private final AnagramMaker anagramSentenceTest = new AnagramMaker();
 
     @Test
-    public void makeAnagram_ShouldReturnAnagram_OfOneWordWithOneNonLetterSymbol() {
-        String inputedSentence = "dty&b";
-        String reversedSentence = "byt&d";
-        assertEquals(reversedSentence, anagramSentenceTest.makeAnagram(inputedSentence));
+    void makeAnagramShouldReturnAnagramOfOneWordWithOneNonLetterSymbol() {
+        final Matcher<String> actual = equalTo(anagramSentenceTest.makeAnagram("dty&b"));
+        final String expected = "byt&d";
+        assertThat(expected, actual);
     }
 
     @Test
-    public void makeAnagram_ShouldReturnAnagram_OfOneWordWithOneLetterSymbol() {
-        String inputedSentence = "!a#&%";
-        String reversedSentence = "!a#&%";
-        assertEquals(reversedSentence, anagramSentenceTest.makeAnagram(inputedSentence));
+    void makeAnagramShouldReturnAnagramOfOneWordWithOneLetterSymbol() {
+        final Matcher<String> actual = equalTo(anagramSentenceTest.makeAnagram("!a#&%"));
+        final String expected = "!a#&%";
+        assertThat(expected, actual);
     }
 
     @Test
-    public void makeAnagram_ShouldReturnAnagram_OfOneWordWithTheSameLetterSymbols() {
-        String inputedSentence = "aaa%a%";
-        String reversedSentence = "aaa%a%";
-        assertEquals(reversedSentence, anagramSentenceTest.makeAnagram(inputedSentence));
+    void makeAnagramShouldReturnAnagramOfOneWordWithTheSameLetterSymbols() {
+        final Matcher<String> actual = equalTo(anagramSentenceTest.makeAnagram("aaa%a%"));
+        final String expected = "aaa%a%";
+        assertThat(expected, actual);
     }
 
     @Test
-    public void makeAnagram_ShouldReturnAnagram_OfOneWordWithTheSameNonLetterSymbols() {
-        String inputedSentence = "!!!!";
-        String reversedSentence = "!!!!";
-        assertEquals(reversedSentence, anagramSentenceTest.makeAnagram(inputedSentence));
+    void makeAnagramShouldReturnAnagramOfOneWordWithTheSameNonLetterSymbols() {
+        final Matcher<String> actual = equalTo(anagramSentenceTest.makeAnagram("!!!!"));
+        final String expected = "!!!!";
+        assertThat(expected, actual);
     }
 
     @Test
-    public void makeAnagram_ShouldReturnAnagram_OfOneWordWithTheDifferentLetterSymbols() {
-        String inputedSentence = "abcdef";
-        String reversedSentence = "fedcba";
-        assertEquals(reversedSentence, anagramSentenceTest.makeAnagram(inputedSentence));
+    void makeAnagramShouldReturnAnagramOfOneWordWithTheDifferentLetterSymbols() {
+        final Matcher<String> actual = equalTo(anagramSentenceTest.makeAnagram("abcdef"));
+        final String expected = "fedcba";
+        assertThat(expected, actual);
     }
 
     @Test
-    public void makeAnagram_ShouldReturnAnagram_OfOneWordWithTheDifferentNonLetterSymbols() {
-        String inputedSentence = "!@#$%";
-        String reversedSentence = "!@#$%";
-        assertEquals(reversedSentence, anagramSentenceTest.makeAnagram(inputedSentence));
+    void makeAnagramShouldReturnAnagramOfOneWordWithTheDifferentNonLetterSymbols() {
+        final Matcher<String> actual = equalTo(anagramSentenceTest.makeAnagram("!@#$%"));
+        final String expected = "!@#$%";
+        assertThat(expected, actual);
     }
 
     @Test
-    public void makeAnagram_ShouldReturnAnagram_OfOneWordWithTheDifferentLetterSymbols_InUpperAndLowerCases() {
-        String inputedSentence = "aBcDeF";
-        String reversedSentence = "FeDcBa";
-        assertEquals(reversedSentence, anagramSentenceTest.makeAnagram(inputedSentence));
+    void makeAnagramShouldReturnAnagramOfOneWordWithTheDifferentLetterSymbolsInUpperAndLowerCases() {
+        final Matcher<String> actual = equalTo(anagramSentenceTest.makeAnagram("aBcDeF"));
+        final String expected = "FeDcBa";
+        assertThat(expected, actual);
     }
 
     @Test
-    public void makeAnagram_ShouldReturnAnagram_OfTwoWordsWithOnlyLetterSymbols() {
-        String inputedSentence = "abcd htr";
-        String reversedSentence = "dcba rth";
-        assertEquals(reversedSentence, anagramSentenceTest.makeAnagram(inputedSentence));
+    void makeAnagramShouldReturnAnagramOfTwoWordsWithOnlyLetterSymbols() {
+        final Matcher<String> actual = equalTo(anagramSentenceTest.makeAnagram("abcd htr"));
+        final String expected = "dcba rth";
+        assertThat(expected, actual);
     }
 
     @Test
-    public void makeAnagram_ShouldReturnAnagram_OfTwoWordsWithOnlyNonLetterSymbols() {
-        String inputedSentence = "!@#$ %^&";
-        String reversedSentence = "!@#$ %^&";
-        assertEquals(reversedSentence, anagramSentenceTest.makeAnagram(inputedSentence));
+    void makeAnagramShouldReturnAnagramOfTwoWordsWithOnlyNonLetterSymbols() {
+        final Matcher<String> actual = equalTo(anagramSentenceTest.makeAnagram("!@#$ %^&"));
+        final String expected = "!@#$ %^&";
+        assertThat(expected, actual);
     }
 
     @Test
-    public void makeAnagram_ShouldReturnAnagram_OfThreeWordsWithDifferentSymbols() {
-        String inputedSentence = "abcd! Fgh% jK#ln";
-        String reversedSentence = "dcba! hgF% nl#Kj";
-        assertEquals(reversedSentence, anagramSentenceTest.makeAnagram(inputedSentence));
+    void makeAnagramShouldReturnAnagramOfThreeWordsWithDifferentSymbols() {
+        final Matcher<String> actual = equalTo(anagramSentenceTest.makeAnagram("abcd! Fgh% jK#ln"));
+        final String expected = "dcba! hgF% nl#Kj";
+        assertThat(expected, actual);
     }
 
     @Test
-    public void makeAnagram_ShouldThrowInputSentenceException_WhenNullPassed() {
-        expectedException.expect(InputSentenceException.class);
-        expectedException.expectMessage("Sentence is null");
-        anagramSentenceTest.makeAnagram(null);
+    void makeAnagramShouldThrowInputSentenceExceptionWhenNullPassed() {
+        InputSentenceException exception = assertThrows(InputSentenceException.class, () ->
+            anagramSentenceTest.makeAnagram(null));
+        assertThat("Sentence is null", equalTo(exception.getMessage()));
     }
 
     @Test
-    public void makeAnagram_ShouldThrowInputSentenceException_WhenEmptySentencePassed() {
-        expectedException.expect(InputSentenceException.class);
-        expectedException.expectMessage("Sentence is empty");
-        anagramSentenceTest.makeAnagram("");
+    void makeAnagramShouldThrowInputSentenceExceptionWhenEmptySentencePassed() {
+        InputSentenceException exception = assertThrows(InputSentenceException.class, () ->
+                anagramSentenceTest.makeAnagram(""));
+        assertThat("Sentence is empty", equalTo(exception.getMessage()));
     }
 
     @Test
-    public void makeAnagram_ShouldThrowInputSentenceException_WhenSpacePassed() {
-        expectedException.expect(InputSentenceException.class);
-        expectedException.expectMessage("Sentence is null or contains only tabulation symbols");
-        anagramSentenceTest.makeAnagram(" ");
+    void makeAnagramShouldThrowInputSentenceExceptionWhenSpacePassed() {
+        InputSentenceException exception = assertThrows(InputSentenceException.class, () ->
+                anagramSentenceTest.makeAnagram(" "));
+        assertThat("Sentence is null or contains only tabulation symbols", equalTo(exception.getMessage()));
     }
 }
